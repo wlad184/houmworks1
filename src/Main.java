8f2569d4b804c7bc85b0f1d0be99474d0cd11e2c
wlad184 <evel184@yandex.ru>
2022-11-08 22:36:52 +0700

    домашка переменные

---
+++ src/Main.java
@@ -0,0 +1,73 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // task 1
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        System.out.println();
+
+        // task 2
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        System.out.println();
+
+        // task 3
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        System.out.println();
+
+        // task 4
+        var friend = 19;
+        System.out.println(friend);
+        friend += 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        System.out.println();
+
+        // task 5
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        System.out.println();
+
+        // task 6
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        var sumWeightBoxer = boxer2 + boxer1;
+        var difWeightBoxer = boxer2 - boxer1;
+        System.out.println("Общий вес бойцов = " + sumWeightBoxer + " кг.");
+        System.out.println("Разница весов бойцов = " + difWeightBoxer + " кг.");
+        System.out.println();
+
+        // task 7
+        difWeightBoxer = boxer2 - boxer1;
+        var difWeightBoxer1 = boxer2 % boxer1;
+        System.out.println(difWeightBoxer);
+        System.out.println(difWeightBoxer1);
+        System.out.println();
+
+        // task 8
+
+
+
+
+    }
+}
